53e74baedb26a8ad23733d8dc054beb99ef0b782
watanabe <rugbymakoto@icloud.com>
2023-01-07 09:21:23 +0900

    deploy

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require_relative "boot"
 
 require "rails/all"
 
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -22,5 +21,6 @@ module Hoge
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    config.assets.initialize_on_precompile = false #追加分
   end
 end

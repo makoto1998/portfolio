62573155dc1efa3c2b5f4428bd94b686a4b255c0
watanabe <rugbymakoto@icloud.com>
2023-01-20 03:03:26 +0900

    update-group_index

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
+  belongs_to :user
   has_many :group_users, dependent: :destroy
   has_many :users, through: :group_users, dependent: :destroy
   validates :name, presence: true, uniqueness: true
